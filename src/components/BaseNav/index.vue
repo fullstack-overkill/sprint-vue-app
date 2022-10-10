86bc4f4978714275df25020e68a09b6d5172672a
fullstack-overkill <christophermms.ctt@gmail.com>
2022-10-10 15:11:54 -0300

    feat: top nav compponent

---
+++ src/components/BaseNav/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav class="header">
+    <BaseUser />
+    <div>
+      <BaseNavMenu />
+      <BaseNavNotifications />
+    </div>
+  </nav>
+</template>
+
+<style lang="stylus" scoped>
+@import 'styles.styl';
+</style>
